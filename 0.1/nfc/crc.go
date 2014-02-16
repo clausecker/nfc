880ed98a86ebb5ba15c65184ec35a74eceef7a2c
Robert Clausecker <fuzxxl@gmail.com>
2014-02-16 02:34:06 +0100

    License code under LGPL version 3

--- 0.1/nfc/crc.go
+++ 0.1/nfc/crc.go
@@ -1,3 +1,17 @@
+// Copyright (c) 2014, Robert Clausecker <fuzxxl@gmail.com>
+//
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as published by the
+// Free Software Foundation, version 3.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+// more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>
+
 // The code in this file is a very direct translation of the code in
 // iso14443-subr.c of the libnfc version 1.7.0 translated to Go for performance
 // reasons.
