bdcf5f9888e3a7aaa8d316442f74f3e34fe56466
Robert Clausecker <fuzxxl@gmail.com>
2014-08-30 19:43:08 +0200

    Release 2.0
    I really hope I don't have to change the API again.

---
+++ 2.0/nfc/device.go
@@ -0,0 +1,460 @@
+// Copyright (c) 2014, Robert Clausecker <fuzxxl@gmail.com>
+//
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as published by the
+// Free Software Foundation, version 3.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+// more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+package nfc
+
+// #include <nfc/nfc.h>
+// #include <stdlib.h>
+import "C"
+import "unsafe"
+import "errors"
+import "fmt"
+
+// NFC device
+type Device struct {
+	d **C.nfc_device
+}
+
+// Return a pointer to the wrapped nfc_device. This is useful if you try to use
+// this wrapper to wrap other C code that builds onto the libnfc.
+func (d Device) Pointer() uintptr {
+	return uintptr(unsafe.Pointer(*d.d))
+}
+
+// Open an NFC device. See documentation of Open() for more details
+func (c *context) open(conn string) (d Device, err error) {
+	c.m.Lock()
+	defer c.m.Unlock()
+	c.initContext()
+
+	cs, err := newConnstring(conn)
+	if err != nil {
+		return
+	}
+
+	defer cs.Free()
+
+	dev := C.nfc_open(c.c, cs.ptr)
+
+	if dev == nil {
+		err = errors.New("cannot open NFC device")
+		return
+	}
+
+	d = Device{&dev}
+	return
+}
+
+// open a connection to an NFC device. If conn is "", the first available device
+// will be used. If this operation fails, check the log on stderr for more
+// details as the libnfc is not particulary verbose to us.
+//
+// Depending on the desired operation mode, the device needs to be configured
+// by using InitiatorInit() or TargetInit(), optionally followed by manual
+// tuning of the parameters if the default parameters are not suiting your
+// goals.
+func Open(conn string) (Device, error) {
+	return theContext.open(conn)
+}
+
+// the error returned by the last operation on d. Every function that wraps some
+// functions operating on an nfc_device should call this function and return the
+// result. This wraps nfc_device_get_last_error.
+func (d Device) LastError() error {
+	if *d.d == nil {
+		return errors.New("device closed")
+	}
+
+	err := Error(C.nfc_device_get_last_error(*d.d))
+
+	if err == 0 {
+		return nil
+	}
+
+	return err
+}
+
+// Close an NFC device.
+func (d Device) Close() error {
+	if *d.d == nil {
+		// closing a closed device is a nop
+		return nil
+	}
+
+	*d.d = nil
+	C.nfc_close(*d.d)
+
+	return nil
+}
+
+// Abort current running command. Some commands (ie. TargetInit()) are blocking
+// functions and will return only in particular conditions (ie. external
+// initiator request). This function attempt to abort the current running
+// command.
+func (d Device) AbortCommand() error {
+	if *d.d == nil {
+		return errors.New("device closed")
+	}
+
+	return Error(C.nfc_abort_command(*d.d))
+}
+
+// Turn NFC device in idle mode. In initiator mode, the RF field is turned off
+// and the device is set to low power mode (if avaible); In target mode, the
+// emulation is stoped (no target available from external initiator) and the
+// device is set to low power mode (if avaible).
+func (d Device) Idle() error {
+	if *d.d == nil {
+		return errors.New("device closed")
+	}
+
+	return Error(C.nfc_idle(*d.d))
+}
+
+// Print information about an NFC device.
+func (d Device) Information() (string, error) {
+	if *d.d == nil {
+		return "", errors.New("device closed")
+	}
+
+	var ptr *C.char
+	buflen := C.nfc_device_get_information_about(*d.d, &ptr)
+
+	if buflen < 0 {
+		return "", Error(buflen)
+	}
+
+	// documentation for nfc_device_get_information_about says that buflen
+	// contains the length of the string that is returned. Apparently, for
+	// some drivers, buflen is always 0 so we disregard it.
+	str := C.GoString(ptr)
+	C.nfc_free(unsafe.Pointer(ptr))
+
+	return str, nil
+}
+
+// Returns the device's connection string. If the device has been closed before,
+// this function returns the empty string.
+func (d Device) Connection() string {
+	if *d.d == nil {
+		return ""
+	}
+
+	cptr := C.nfc_device_get_connstring(*d.d)
+	return C.GoString(cptr)
+}
+
+// Returns the device's name. This information is not enough to uniquely
+// determine the device.
+func (d Device) String() string {
+	if *d.d == nil {
+		return ""
+	}
+
+	cptr := C.nfc_device_get_name(*d.d)
+	return C.GoString(cptr)
+}
+
+// Return Go code that could be used to reproduce this device.
+func (d Device) GoString() string {
+	if *d.d == nil {
+		return "nil"
+	}
+
+	return fmt.Sprintf("nfc.Open(%q)", d.Connection())
+}
+
+// Set a device's integer-property value. Returns nil on success, otherwise an
+// error. See integer constants in this package for possible properties.
+func (d Device) SetPropertyInt(property, value int) error {
+	if *d.d == nil {
+		return errors.New("device closed")
+	}
+
+	err := C.nfc_device_set_property_int(*d.d, C.nfc_property(property), C.int(value))
+
+	if err != 0 {
+		return Error(err)
+	}
+
+	return nil
+}
+
+// Set a device's boolean-property value. Returns nil on success, otherwise an
+// error. See integer constants in this package for possible properties.
+func (d Device) SetPropertyBool(property int, value bool) error {
+	if *d.d == nil {
+		return errors.New("device closed")
+	}
+
+	err := C.nfc_device_set_property_bool(*d.d, C.nfc_property(property), C.bool(value))
+
+	if err != 0 {
+		return Error(err)
+	}
+
+	return nil
+}
+
+// Get supported modulations. Returns a slice of supported modulations or an
+// error. Pass either TARGET or INITIATOR as mode. This function wraps
+// nfc_device_get_supported_modulation()
+func (d Device) SupportedModulations(mode int) ([]int, error) {
+	if *d.d == nil {
+		return nil, errors.New("device closed")
+	}
+
+	// The documentation inside the libnfc is a bit unclear on how the
+	// array returned through supported_mt is to be threated. The code
+	// itself suggest that it points to an array of entries terminated with
+	// UNDEFINED = 0.
+	var mt_arr *C.nfc_modulation_type
+	ret := C.nfc_device_get_supported_modulation(*d.d, C.nfc_mode(mode), &mt_arr)
+	if ret != 0 {
+		return nil, Error(ret)
+	}
+
+	mods := []int{}
+	type mod C.nfc_modulation_type
+	ptr := unsafe.Pointer(mt_arr)
+
+	for *(*mod)(ptr) != 0 {
+		mods = append(mods, int(*(*mod)(ptr)))
+		ptr = unsafe.Pointer(uintptr(ptr) + unsafe.Sizeof(*mt_arr))
+	}
+
+	return mods, nil
+}
+
+// Get suported baud rates. Returns either a slice of supported baud rates or an
+// error. This function wraps nfc_device_get_supported_baud_rate().
+func (d Device) SupportedBaudRates(modulationType int) ([]int, error) {
+	if *d.d == nil {
+		return nil, errors.New("device closed")
+	}
+
+	// The documentation inside the libnfc is a bit unclear on how the
+	// array returned through supported_mt is to be threated. The code
+	// itself suggest that it points to an array of entries terminated with
+	// UNDEFINED = 0.
+	var br_arr *C.nfc_baud_rate
+	ret := C.nfc_device_get_supported_baud_rate(
+		*d.d,
+		C.nfc_modulation_type(modulationType),
+		&br_arr,
+	)
+	if ret != 0 {
+		return nil, Error(ret)
+	}
+
+	brs := []int{}
+	type br C.nfc_baud_rate
+	ptr := unsafe.Pointer(br_arr)
+
+	for *(*br)(ptr) != 0 {
+		brs = append(brs, int(*(*br)(ptr)))
+		ptr = unsafe.Pointer(uintptr(ptr) + unsafe.Sizeof(*br_arr))
+	}
+
+	return brs, nil
+}
+
+// Initialize NFC device as an emulated tag. n contains the received byte count
+// on success, or is meaningless on error. The current implementation will
+// return the libnfc error code in case of error, but this is subject to change.
+// The returned target tt will be the result of the modifications
+// nfc_target_init() applies to t. Such modifications might happen if you set
+// an Baud or DepMode to UNDEFINED. The fields will be updated with concrete
+// values. timeout contains a timeout in milliseconds.
+//
+// This function initializes NFC device in target mode in order to emulate a tag
+// as the specified target t.
+//  - Crc is handled by the device (HANDLE_CRC = true)
+//  - Parity is handled the device (HANDLE_PARITY = true)
+//  - Cryto1 cipher is disabled (ACTIVATE_CRYPTO1 = false)
+//  - Auto-switching in ISO14443-4 mode is enabled (AUTO_ISO14443_4 = true)
+//  - Easy framing is disabled (EASY_FRAMING = false)
+//  - Invalid frames are not accepted (ACCEPT_INVALID_FRAMES = false)
+//  - Multiple frames are not accepted (ACCEPT_MULTIPLE_FRAMES = false)
+//  - RF field is dropped
+//
+// Warning: Be aware that this function will wait (hang) until a command is
+// received that is not part of the anti-collision. The RATS command for example
+// would wake up the emulator. After this is received, the send and receive
+// functions can be used.
+//
+// If timeout equals to 0, the function blocks indefinitely (until an error is
+// raised or function is completed). If timeout equals to -1, the default
+// timeout will be used.
+func (d Device) TargetInit(t Target, rx []byte, timeout int) (n int, tt Target, err error) {
+	if *d.d == nil {
+		return ESOFT, t, errors.New("device closed")
+	}
+
+	tar := (*C.nfc_target)(unsafe.Pointer(t.Marshall()))
+	defer C.free(unsafe.Pointer(tar))
+
+	n = int(C.nfc_target_init(
+		*d.d, tar,
+		(*C.uint8_t)(&rx[0]), C.size_t(len(rx)),
+		C.int(timeout),
+	))
+
+	if n < 0 {
+		err = Error(n)
+	}
+
+	tt = unmarshallTarget(tar)
+	return
+}
+
+// Send bytes and APDU frames. n contains the sent byte count on success, or is
+// meaningless on error. The current implementation will return the libnfc
+// error code in case of error, but this is subject to change. timeout contains
+// a timeout in milliseconds.
+//
+// This function make the NFC device (configured as  target) send byte frames
+// (e.g. APDU responses) to the initiator.
+//
+// If timeout equals to 0, the function blocks indefinitely (until an error is
+// raised or function is completed). If timeout equals to -1, the default
+// timeout will be used.
+func (d Device) TargetSendBytes(tx []byte, timeout int) (n int, err error) {
+	if *d.d == nil {
+		return ESOFT, errors.New("device closed")
+	}
+
+	n = int(C.nfc_target_send_bytes(
+		*d.d,
+		(*C.uint8_t)(&tx[0]), C.size_t(len(tx)),
+		C.int(timeout),
+	))
+
+	if n < 0 {
+		err = Error(n)
+	}
+
+	return
+}
+
+// Receive bytes and APDU frames. n contains the received byte count on success,
+// or is meaningless on error. The current implementation will return the libnfc
+// error code in case of error, but this is subject to change. timeout contains
+// a timeout in milliseconds.
+//
+// This function retrieves bytes frames (e.g. ADPU) sent by the initiator to the
+// NFC device (configured as target).
+//
+// If timeout equals to 0, the function blocks indefinitely (until an error is
+// raised or function is completed). If timeout equals to -1, the default
+// timeout will be used.
+func (d Device) TargetReceiveBytes(rx []byte, timeout int) (n int, err error) {
+	if *d.d == nil {
+		return ESOFT, errors.New("device closed")
+	}
+
+	n = int(C.nfc_target_receive_bytes(
+		*d.d,
+		(*C.uint8_t)(&rx[0]), C.size_t(len(rx)),
+		C.int(timeout),
+	))
+
+	if n < 0 {
+		err = Error(n)
+	}
+
+	return
+}
+
+// Send raw bit-frames. Returns sent bits count on success, n contains the sent
+// bit count on success, or is meaningless on error. The current implementation
+// will return the libnfc error code in case of error, but this is subject to
+// change. txPar has to have the same length as tx, an error will occur if this
+// invariant does not hold.
+//
+// tx contains a byte slice of the frame that needs to be transmitted. txLength
+// contains its length in bits. txPar contains a byte slice of the corresponding
+// parity bits needed to send per byte.
+//
+// his function can be used to transmit (raw) bit-frames to the initiator using
+// the specified NFC device (configured as target).
+func (d Device) TargetSendBits(tx []byte, txPar []byte, txLength uint) (n int, err error) {
+	if *d.d == nil {
+		return ESOFT, errors.New("device closed")
+	}
+
+	if len(tx) != len(txPar) {
+		return ESOFT, errors.New("invariant doesn't hold")
+	}
+
+	if uint(len(tx))*8 < txLength {
+		return ESOFT, errors.New("slice shorter than specified bit count")
+	}
+
+	n = int(C.nfc_target_send_bits(
+		*d.d,
+		(*C.uint8_t)(&tx[0]),
+		C.size_t(txLength),
+		(*C.uint8_t)(&txPar[0]),
+	))
+
+	if n < 0 {
+		err = Error(n)
+	}
+
+	return
+}
+
+// Receive bit-frames. Returns received bits count on success, n contains the
+// received bit count on success, or is meaningless on error. The current
+// implementation will return the libnfc error code in case of error, but this
+// is subject to change. rxPar has to have the same length as rx, an error will
+// occur if this invariant does not hold.
+//
+// rx contains a byte slice of the frame that you want to receive. rxLength
+// contains its length in bits. rxPar contains a byte slice of the corresponding
+// parity bits received per byte.
+//
+// This function makes it possible to receive (raw) bit-frames. It returns all
+// the messages that are stored in the FIFO buffer of the PN53x chip. It
+// does not require to send any frame and thereby could be used to snoop frames
+// that are transmitted by a nearby initiator. Check out the
+// ACCEPT_MULTIPLE_FRAMES configuration option to avoid losing transmitted
+// frames.
+func (d Device) TargetTransceiveBits(rx []byte, rxPar []byte, rxLength uint) (n int, err error) {
+	if *d.d == nil {
+		return ESOFT, errors.New("device closed")
+	}
+
+	if len(rx) != len(rxPar) {
+		return ESOFT, errors.New("invariant doesn't hold")
+	}
+
+	if uint(len(rx))*8 < rxLength {
+		return ESOFT, errors.New("slice shorter than specified bit count")
+	}
+
+	n = int(C.nfc_target_receive_bits(
+		*d.d,
+		(*C.uint8_t)(&rx[0]),
+		C.size_t(rxLength),
+		(*C.uint8_t)(&rxPar[0]),
+	))
+
+	if n < 0 {
+		err = Error(n)
+	}
+
+	return
+}
---
+++ 2.0/nfc/marshall.c
@@ -0,0 +1,247 @@
+/*-
+ * Copyright (c) 2014, Robert Clausecker <fuzxxl@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+#include <string.h>
+#include "marshall.h"
+
+/* see marshall.h for what this code does */
+
+extern int
+getModulationType(const nfc_target *nt)
+{
+	return (nt->nm.nmt);
+}
+
+extern void
+unmarshallDEPTarget(struct DEPTarget *dt, const nfc_target *nt)
+{
+	const nfc_dep_info *di = &nt->nti.ndi;
+
+	memcpy(dt->NFCID3, di->abtNFCID3, sizeof(dt->NFCID3));
+	dt->DID = di->btDID;
+	dt->BS = di->btBS;
+	dt->BR = di->btBR;
+	dt->TO = di->btTO;
+	dt->PP = di->btPP;
+	memcpy(dt->GB, di->abtGB, sizeof(dt->GB));
+	dt->GBLen = di->szGB;
+	dt->DepMode = di->ndm;
+
+	dt->Baud = nt->nm.nbr;
+}
+
+extern void
+marshallDEPTarget(nfc_target *nt, const struct DEPTarget *dt)
+{
+	nfc_dep_info *di = &nt->nti.ndi;
+
+	memcpy(di->abtNFCID3, dt->NFCID3, sizeof(dt->NFCID3));
+	di->btDID = dt->DID;
+	di->btBS = dt->BS;
+	di->btBR = dt->BR;
+	di->btTO = dt->TO;
+	di->btPP = dt->PP;
+	memcpy(di->abtGB, dt->GB, sizeof(dt->GB));
+	di->szGB = dt->GBLen;
+	di->ndm = dt->DepMode;
+
+	nt->nm.nbr = dt->Baud;
+	nt->nm.nmt = NMT_DEP;
+}
+
+extern void
+unmarshallISO14443aTarget(struct ISO14443aTarget *it, const nfc_target *nt)
+{
+	const nfc_iso14443a_info *ii = &nt->nti.nai;
+
+	memcpy(it->Atqa, ii->abtAtqa, sizeof(it->Atqa));
+	it->Sak = ii->btSak;
+	it->UIDLen = ii->szUidLen;
+	memcpy(it->UID, ii->abtUid, sizeof(it->UID));
+	it->AtsLen = ii->szAtsLen;
+	memcpy(it->Ats, ii->abtAts, sizeof(it->Ats));
+
+	it->Baud = nt->nm.nbr;
+}
+
+extern void
+marshallISO14443aTarget(nfc_target *nt, const struct ISO14443aTarget *it)
+{
+	nfc_iso14443a_info *ii = &nt->nti.nai;
+
+	memcpy(ii->abtAtqa, it->Atqa, sizeof(it->Atqa));
+	ii->btSak = it->Sak;
+	ii->szUidLen = it->UIDLen;
+	memcpy(ii->abtUid, it->UID, sizeof(it->UID));
+	ii->szAtsLen = it->AtsLen;
+	memcpy(ii->abtAts, it->Ats, sizeof(it->Ats));
+
+	nt->nm.nbr = it->Baud;
+	nt->nm.nmt = NMT_ISO14443A;
+}
+
+extern void
+unmarshallFelicaTarget(struct FelicaTarget *ft, const nfc_target *nt)
+{
+	const nfc_felica_info *fi = &nt->nti.nfi;
+
+	ft->Len = fi->szLen;
+	ft->ResCode = fi->btResCode;
+	memcpy(ft->ID, fi->abtId, sizeof(ft->ID));
+	memcpy(ft->Pad, fi->abtPad, sizeof(ft->Pad));
+	memcpy(ft->SysCode, fi->abtSysCode, sizeof(ft->SysCode));
+
+	ft->Baud = nt->nm.nbr;
+}
+
+extern void
+marshallFelicaTarget(nfc_target *nt, const struct FelicaTarget *ft)
+{
+	nfc_felica_info *fi = &nt->nti.nfi;
+
+	fi->szLen = ft->Len;
+	fi->btResCode = ft->ResCode;
+	memcpy(fi->abtId, ft->ID, sizeof(ft->ID));
+	memcpy(fi->abtPad, ft->Pad, sizeof(ft->Pad));
+	memcpy(fi->abtSysCode, ft->SysCode, sizeof(ft->SysCode));
+
+	nt->nm.nbr = ft->Baud;
+	nt->nm.nmt = NMT_FELICA;
+}
+
+extern void
+unmarshallISO14443bTarget(struct ISO14443bTarget *it, const nfc_target *nt)
+{
+	const nfc_iso14443b_info *ii = &nt->nti.nbi;
+
+	memcpy(it->Pupi, ii->abtPupi, sizeof(it->Pupi));
+	memcpy(it->ApplicationData, ii->abtApplicationData, sizeof(it->ApplicationData));
+	memcpy(it->ProtocolInfo, ii->abtProtocolInfo, sizeof(it->ProtocolInfo));
+	it->CardIdentifier = ii->ui8CardIdentifier;
+
+	it->Baud = nt->nm.nbr;
+}
+
+extern void
+marshallISO14443bTarget(nfc_target *nt, const struct ISO14443bTarget *it)
+{
+	nfc_iso14443b_info *ii = &nt->nti.nbi;
+
+	memcpy(ii->abtPupi, it->Pupi, sizeof(it->Pupi));
+	memcpy(ii->abtApplicationData, it->ApplicationData, sizeof(it->ApplicationData));
+	memcpy(ii->abtProtocolInfo, it->ProtocolInfo, sizeof(it->ProtocolInfo));
+	ii->ui8CardIdentifier = it->CardIdentifier;
+
+	nt->nm.nbr = it->Baud;
+	nt->nm.nmt = NMT_ISO14443B;
+}
+
+extern void
+unmarshallISO14443biTarget(struct ISO14443biTarget *it, const nfc_target *nt)
+{
+	const nfc_iso14443bi_info *ii = &nt->nti.nii;
+
+	memcpy(it->DIV, ii->abtDIV, sizeof(it->DIV));
+	it->VerLog = ii->btVerLog;
+	it->Config = ii->btConfig;
+	it->AtrLen = ii->szAtrLen;
+	memcpy(it->Atr, ii->abtAtr, sizeof(it->Atr));
+
+	it->Baud = nt->nm.nbr;
+}
+
+extern void
+marshallISO14443biTarget(nfc_target *nt, const struct ISO14443biTarget *it)
+{
+	nfc_iso14443bi_info *ii = &nt->nti.nii;
+
+	memcpy(ii->abtDIV, it->DIV, sizeof(it->DIV));
+	ii->btVerLog = it->VerLog;
+	ii->btConfig = it->Config;
+	ii->szAtrLen = it->AtrLen;
+	memcpy(ii->abtAtr, it->Atr, sizeof(it->Atr));
+
+	nt->nm.nbr = it->Baud;
+	nt->nm.nmt = NMT_ISO14443BI;
+}
+
+extern void
+unmarshallISO14443b2srTarget(struct ISO14443b2srTarget *it, const nfc_target *nt)
+{
+	const nfc_iso14443b2sr_info *ii = &nt->nti.nsi;
+
+	memcpy(it->UID, ii->abtUID, sizeof(it->UID));
+
+	it->Baud = nt->nm.nbr;
+}
+
+extern void
+marshallISO14443b2srTarget(nfc_target *nt, const struct ISO14443b2srTarget *it)
+{
+	nfc_iso14443b2sr_info *ii = &nt->nti.nsi;
+
+	memcpy(ii->abtUID, it->UID, sizeof(it->UID));
+
+	nt->nm.nbr = it->Baud;
+	nt->nm.nmt = NMT_ISO14443B2SR;
+}
+
+extern void
+unmarshallISO14443b2ctTarget(struct ISO14443b2ctTarget *it, const nfc_target *nt)
+{
+	const nfc_iso14443b2ct_info *ii = &nt->nti.nci;
+
+	memcpy(it->UID, ii->abtUID, sizeof(it->UID));
+	it->ProdCode = ii->btProdCode;
+	it->FabCode = ii->btFabCode;
+
+	it->Baud = nt->nm.nbr;
+}
+
+extern void
+marshallISO14443b2ctTarget(nfc_target *nt, const struct ISO14443b2ctTarget *it)
+{
+	nfc_iso14443b2ct_info *ii = &nt->nti.nci;
+
+	memcpy(ii->abtUID, it->UID, sizeof(it->UID));
+	ii->btProdCode = it->ProdCode;
+	ii->btFabCode = it->FabCode;
+
+	nt->nm.nbr = it->Baud;
+	nt->nm.nmt = NMT_ISO14443B2CT;
+}
+
+extern void
+unmarshallJewelTarget(struct JewelTarget *jt, const nfc_target *nt)
+{
+	const nfc_jewel_info *ji = &nt->nti.nji;
+
+	memcpy(jt->SensRes, ji->btSensRes, sizeof(jt->SensRes));
+	memcpy(jt->ID, ji->btId, sizeof(jt->ID));
+
+	jt->Baud = nt->nm.nbr;
+}
+
+extern void
+marshallJewelTarget(nfc_target *nt, const struct JewelTarget *jt)
+{
+	nfc_jewel_info *ji = &nt->nti.nji;
+
+	memcpy(ji->btSensRes, jt->SensRes, sizeof(jt->SensRes));
+	memcpy(ji->btId, jt->ID, sizeof(jt->ID));
+
+	nt->nm.nbr = jt->Baud;
+	nt->nm.nmt = NMT_JEWEL;
+}
