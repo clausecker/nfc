301d11c94e86c3583ba0eefa64d3b652135d7ac7
Robert Clausecker <fuzxxl@gmail.com>
2015-06-29 16:14:58 +0200

    Bump year in copyright notice to 2015 for files changed in 2015

--- dev/nfc/initiator.go
+++ dev/nfc/initiator.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2014, Robert Clausecker <fuzxxl@gmail.com>
+// Copyright (c) 2014, 2015 Robert Clausecker <fuzxxl@gmail.com>
 //
 // This program is free software: you can redistribute it and/or modify it
 // under the terms of the GNU Lesser General Public License as published by the
