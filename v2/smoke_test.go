922661fd5ffa1a8e0882fd2bc0e837bcf12e7b4b
Robert Clausecker <fuz@fuz.su>
2024-03-07 22:48:48 +0100

    smoke_test.go: add test for Device.InitiatorListPassiveTargets()

--- v2/smoke_test.go
+++ v2/smoke_test.go
@@ -11,3 +11,35 @@ func TestListDevices(t *testing.T) {
 		t.Log("ListDevices():", devs)
 	}
 }
+
+var testModulations []Modulation = []Modulation{
+	{Type: ISO14443a, BaudRate: Nbr106},
+	{Type: Felica, BaudRate: Nbr212},
+	{Type: Felica, BaudRate: Nbr424},
+	{Type: ISO14443b, BaudRate: Nbr106},
+	{Type: ISO14443bi, BaudRate: Nbr106},
+	{Type: ISO14443b2sr, BaudRate: Nbr106},
+	{Type: ISO14443b2ct, BaudRate: Nbr106},
+	{Type: ISO14443biClass, BaudRate: Nbr106},
+	{Type: Jewel, BaudRate: Nbr106},
+	{Type: Barcode, BaudRate: Nbr106},
+}
+
+// Open the first device and list all tags
+func TestInitiatorListPassiveTargets(t *testing.T) {
+	dev, err := Open("")
+	defer dev.Close()
+	if err != nil {
+		t.Skip("Cannot open device:", err)
+	}
+
+	for i := range testModulations {
+		targets, err := dev.InitiatorListPassiveTargets(testModulations[i])
+		if err != nil {
+			t.Log(dev.GoString(), ".InitiatorListPassiveTargets:", err)
+			t.FailNow()
+		}
+
+		t.Log(dev.Connection(), "/", testModulations[i], ":", targets)
+	}
+}
